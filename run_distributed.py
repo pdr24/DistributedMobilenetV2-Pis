aedadc350df93268fd458cc349e79b841219816d
Durga Rajarajan <pragathidurga24@gmail.com>
2025-03-20 23:33:18 -0500

    fixed run_distributed with cc as pi username

--- run_distributed.py
+++ run_distributed.py
@@ -10,12 +10,12 @@ PI0_PATH="/home/cc/programs/DistributedMobilenetV2-Pis/distributed_mobilenetv2.p
 PI1_PATH="/home/cc/programs/DistributedMobilenetV2-Pis/distributed_mobilenetv2.py"
 
 # run on pi 0 (rank 0)
-ssh pi@$PI0_IP "
+ssh cc@$PI0_IP "
   torchrun --nnodes=2 --nproc_per_node=1 --node_rank=0 --master_addr='$MASTER_ADDR' --master_port=29500 $PI0_PATH
 " &
 
 # run on pi 1 (rank 1)
-ssh pi@$PI1_IP "
+ssh cc@$PI1_IP "
   torchrun --nnodes=2 --nproc_per_node=1 --node_rank=1 --master_addr='$MASTER_ADDR' --master_port=29500 $PI1_PATH
 " &
 
